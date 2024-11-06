33dac7509f1e5d4e137672fc3ad8550e1efe28a0
Arnab20k-Peakflo <arnab.chatterjee@peakflo.co>
2024-11-06 22:49:31 +0530

    added feedbacks and testimonials

---
+++ src/app/feedbacks/page.jsx
@@ -0,0 +1,39 @@
+"use client";
+import React from "react";
+
+export default function page() {
+  React.useEffect(() => {
+    try {
+      const script = document.createElement("script");
+      script.src =
+        "https://cdn.jsdelivr.net/gh/ArnabChatterjee20k/feedbackio-widget@latest/wall-of-fame-cdn/wall-bundle.js";
+      script.type = "module";
+      document.body.appendChild(script);
+
+      // Load external stylesheet
+      const link = document.createElement("link");
+      link.href =
+        "https://cdn.jsdelivr.net/gh/ArnabChatterjee20k/feedbackio-widget@master/wall-of-fame-cdn/wall-style.css";
+      link.rel = "stylesheet";
+      document.head.appendChild(link);
+
+      return () => {
+        document.body.removeChild(script);
+        document.head.removeChild(link);
+      };
+    } catch (error) {
+      alert("some error occured");
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+  return (
+    <>
+      <div
+        id="feedbackio-wall-of-fame"
+        data-spaceid="6720788b0025006c6d47"
+      ></div>
+    </>
+  );
+}
